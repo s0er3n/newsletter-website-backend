0856cf481203cfea0c0e1b88223fce3ba86ceef4
tilman <tilman.grueger@web.de>
2021-06-09 17:07:30 +0200

    g

--- newsletter_api/db/mongodb.py
+++ newsletter_api/db/mongodb.py
@@ -23,22 +23,37 @@ def connect_to_server():
 
 def creat_letter(dict):
     '''dict (inc user_id) mit änderung am letter, returns post_id'''
-    posts = connect_to_server()
-    post_id = posts.insert_one(dict).inserted_id
-    return post_id
+    try:
+        posts = connect_to_server()
+        post_id = posts.insert_one(dict).inserted_id
+        return post_id
+
+    except Exception:
+        return "newsletter konnte nicht in der db erstellt werden"
 
 def read_letters(user_id, post_id = None):
-    posts = connect_to_server()
-    if post_id:
-        return posts.find_one({"_id": ObjectId(post_id), "user_id": user_id})
-    else:
-        newsletterlist = []
-        for post in posts.find({"user_id": user_id}):
-            newsletterlist.append(post)
-        return newsletterlist
+    '''wenn nur user_id, werden alle newsletter sonst der 
+    spezifische newsletter in einer List returned
+    '''
+    try:
+        posts = connect_to_server()
+        if post_id:
+            return [posts.find_one({"_id": ObjectId(post_id), "user_id": user_id})]
+        else:
+            newsletterlist = []
+            for post in posts.find({"user_id": user_id}):
+                newsletterlist.append(post)
+            return newsletterlist
+
+    except Exception:
+        return "newsletter konnte nicht in der db gefunden werden"
 
 def delete_letter(post_id, user_id):
-    posts = connect_to_server()
-    x = posts.delete_many({"_id": ObjectId(post_id), "user_id": user_id})
-    return x.deleted_count
+    try:
+        posts = connect_to_server()
+        x = posts.delete_many({"_id": ObjectId(post_id), "user_id": user_id})
+        return x.deleted_count
+
+    except Exception:
+        return "newsletter konnte nicht in der db gelöscht werden"
 
