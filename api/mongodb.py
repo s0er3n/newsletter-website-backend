3ae0da00d77f87c1967dcad04508e2877781fdbb
Sören Michaels <soeren.michaels@gmail.com>
2021-07-08 16:19:01 +0200

    added newsletters db automatically

--- api/mongodb.py
+++ api/mongodb.py
@@ -7,20 +7,23 @@ load_dotenv()
 
 from rethinkdb import RethinkDB
 r = RethinkDB()
-r.connect( "rethink", 28015).repl()
+connection = r.connect( "rethink", 28015)
 newsletters = r.db("test").table("newsletters")
-
+try:
+    r.db("test").table_create("newsletters").run(connection)
+except:
+    print("table already there")
 
 def create_newsletter(newsletter: Newsletter) -> str:
-    answer = newsletters.insert(asdict(newsletter)).run()
+    answer = newsletters.insert(asdict(newsletter)).run(connection)
     return str(answer)
 
 def get_newsletters(user_id: strawberry.ID, post_id: str = None):
     if post_id:
 
-        cursor = newsletters.filter(r.row["user_id"] == user_id and r.row["id"] == post_id).run()
+        cursor = newsletters.filter(r.row["user_id"] == user_id and r.row["id"] == post_id).run(connection)
     else:
-        cursor = newsletters.filter(r.row["user_id"] == user_id).run()
+        cursor = newsletters.filter(r.row["user_id"] == user_id).run(connection)
 
     newsletterlist = []
     for newsletter in cursor:
@@ -29,10 +32,10 @@ def get_newsletters(user_id: strawberry.ID, post_id: str = None):
     return newsletterlist
 
 def delete_newsletter(newsletter_id):
-    return str(newsletters.filter(r.row["id"] == newsletter_id).delete().run())
+    return str(newsletters.filter(r.row["id"] == newsletter_id).delete().run(connection))
 
 
 def change_newsletter(newsletter_id, new_newsletter):
-    return str(newsletters.filter(r.row["id"] == newsletter_id).update(asdict(new_newsletter)).run())
+    return str(newsletters.filter(r.row["id"] == newsletter_id).update(asdict(new_newsletter)).run(connection))
 
 
